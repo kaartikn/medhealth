cea2a1788c3aebedf7fc64f1c16d186b3c076da1
Kaartik <kaartik2000@gmail.com>
2024-07-16 07:02:26 +0800

    Nullified link errors

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -20,9 +20,9 @@ const NavbarComponent = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#dosage">Dosage</Nav.Link>
+            <Nav.Link>Home</Nav.Link>
+            <Nav.Link>Features</Nav.Link>
+            <Nav.Link>Dosage</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
